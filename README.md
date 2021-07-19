a6c1b9171b0c6ece6be916df9dfc07bc38c607f5
admin@ammar <admin@ammar.awash>
2021-07-19 09:29:28 +0300

    added the files

---
+++ README.md
@@ -0,0 +1,35 @@
+# بسم الله الرحمن الرحيم 
+
+
+
+# a library inspired by wow.js and i call it tarn.js
+ its smoother of wow.js bcz i dont use a lot of code
+ and the idea is deffrent than wow.js
+ the idea is once the element get visible in the client borwser it will take of the class tarn and replace it with any thing you want 
+ and a little secret sence you came here
+ i made tran a shortcut for traniton
+ dont till anyone
+ by the time of making this repo
+ before we get started 
+ if you like it a star will be the best
+
+
+ # using
+ you can use it by importing it from cdn links 
+ or download [it](http://127.0.0.1:5500/tran/README.md)
+ you should import it with
+ in the html 
+ first thing you wanna add is the js file and 
+ * <script defer src="tran.js"></script>
+ and the css file suppose to be imported in the head tag
+ * <link rel="stylesheet" href="tran.css">
+
+ and both of their links will change debends if you are importing it from cdn or you download it 
+
+
+
+
+ # if there is some thing wrong ...
+ if there is some thing wrong please pull a pr
+ and we will see
+ i guess we are pretty done
---
+++ tran.js
@@ -0,0 +1,92 @@
+
+
+
+
+
+
+
+// we get all of the elements we want to do our effect on
+// in our case we made the class .tran
+const elements = document.querySelectorAll('.tran')
+
+
+// and we choose them one by one to do our effect on them 
+window.onscroll = ()=>{
+    elements.forEach(element => {
+        if(element.getBoundingClientRect().y <= height){
+            element.classList.remove('tran')
+            element.classList.add(element.getAttribute('tarn-data-class-to'))
+        }
+    });
+}
+
+
+
+
+// we want the effect to remove the class when the element is in the middle of the screen
+// so we can set a height variable 
+// and it will take the height of the screen and cut it into to 
+// and we keep upadting this variable by on resize function on the window
+// and also iam going to to put the cut on external variable
+let cutInto = 1.5
+let height = window.innerHeight / cutInto
+window.onresize = ()=>{
+    // and we just assign the varaible to another value again
+    height = window.innerHeight / cutInto
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+    * as we acctepted 
+    * i really scared if i lose her
+    * iam sorry if i did something wrong
+    * i hope i die
+    * its the first time i find some one 
+    * kinda understands me
+    * in every shit i say
+    * in ever type i write she understands
+    * i just like her
+*/
+
